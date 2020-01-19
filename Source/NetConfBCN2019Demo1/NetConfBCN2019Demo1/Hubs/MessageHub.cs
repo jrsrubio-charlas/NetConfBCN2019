e0c264349a2af388c28638f3bf83b8ab14f64cb7
Jose Ramos Sánchez <jramos@plainconcepts.com>
2020-01-19 23:14:10 +0100

    Initial commit:  * Presentation  * Code

---
+++ Source/NetConfBCN2019Demo1/NetConfBCN2019Demo1/Hubs/MessageHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace NetConfBCN2019Demo1.Hubs
+{
+    public class MessageHub : Hub
+    {
+        //Aquí se implentarían los métodos para ser llamados desde el cliente, por ejemplo en un chat ;-)
+        public Task BroadcastMessage(string message)
+        {
+            return Clients.All.SendAsync("new-message", message);
+        }
+    }
+}
---
+++ Source/NetConfBCN2019Demo1Azure/NetConfBCN2019Demo1Azure/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using NetConfBCN2019Demo1.Hubs;
+
+namespace NetConfBCN2019Demo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        IHubContext<MessageHub> context;
+        public HomeController(IHubContext<MessageHub> hub)
+        {
+            context = hub;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public void AddMessage(string message)
+        {
+            context.Clients.All.SendAsync("new-message", message);         
+        }
+
+        
+    }
+}
